6295f26e441197c55bb55d118f0d7b6c0f254636
System <System>
2020-02-17 18:07:15 +1300

    Setting script metadata: Check Blog Online.ps1

--- .ua/ANZPSUG Demo/Check Blog Online.Meta.ps1
+++ .ua/ANZPSUG Demo/Check Blog Online.Meta.ps1
@@ -7,7 +7,7 @@ $ScriptInfo.ManualTime = 30
 $ScriptInfo.Tags = @()
 $ScriptInfo.Schedules = $Schedules
 $ScriptInfo.FullPath = 'ANZPSUG Demo/Check Blog Online.ps1'
-$ScriptInfo.RequiredPowerShellVersion = ''
+$ScriptInfo.RequiredPowerShellVersion = '7.0.0-rc.2'
 $ScriptInfo.ErrorAction = 'Stop'
 $ScriptInfo.DisableManualInvocation = $False
 $ScriptInfo
