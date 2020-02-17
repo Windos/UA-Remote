7908a9b38039cba9d0d2ca574030041473cd443b
System <System>
2020-02-17 18:16:20 +1300

    Setting script metadata: Progress.ps1

---
+++ .ua/Progress.Meta.ps1
@@ -0,0 +1,13 @@
+$ScriptInfo = [UniversalAutomation.ScriptInfo]::new()
+$Schedules = @()
+
+$ScriptInfo.Name = 'Progress.ps1'
+$ScriptInfo.Description = ''
+$ScriptInfo.ManualTime = 120
+$ScriptInfo.Tags = @()
+$ScriptInfo.Schedules = $Schedules
+$ScriptInfo.FullPath = 'Progress.ps1'
+$ScriptInfo.RequiredPowerShellVersion = ''
+$ScriptInfo.ErrorAction = 'Stop'
+$ScriptInfo.DisableManualInvocation = $False
+$ScriptInfo
