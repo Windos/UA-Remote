80540bb622b30002f0461efe633dd33e5485f145
System <System>
2020-02-17 17:52:18 +1300

    Setting script metadata: Check Blog Online.ps1

---
+++ .ua/ANZPSUG Demo/Check Blog Online.Meta.ps1
@@ -0,0 +1,13 @@
+$ScriptInfo = [UniversalAutomation.ScriptInfo]::new()
+$Schedules = @()
+
+$ScriptInfo.Name = 'Check Blog Online.ps1'
+$ScriptInfo.Description = ''
+$ScriptInfo.ManualTime = 30
+$ScriptInfo.Tags = @()
+$ScriptInfo.Schedules = $Schedules
+$ScriptInfo.FullPath = 'ANZPSUG Demo/Check Blog Online.ps1'
+$ScriptInfo.RequiredPowerShellVersion = ''
+$ScriptInfo.ErrorAction = 'Stop'
+$ScriptInfo.DisableManualInvocation = $False
+$ScriptInfo
